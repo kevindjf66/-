3d7cd714321298618d32808faa3279198b024874
Kevin Deng <31214837+kevindjf66@users.noreply.github.com>
2017-09-17 10:25:41 -0400

    Update R_code.R

--- R_code.R
+++ R_code.R
@@ -1,34 +1,35 @@
-
+#change the path to where the data is
 setwd("E://R language//DATA")
-
+#read the data file
 DATA=read.csv("data.csv")
-
+#display the first 5 lines of data file
 head(DATA)
-
+#import ggplot2
 library(ggplot2)
+#Display China's investment in south-eastern Asia Countries from 2010 to 2014
 ggplot(data = DATA, aes(x=Year, y=Investment, fill=Countries)) +
   geom_bar(stat='identity', position="dodge")
 
-
+#import dylyr package
 library(dplyr)
-
+#Built the table of China's investment in Singapore
 Singapore <- DATA %>%
-  filter(Countries=="ÐÂ¼ÓÆÂ")
+  filter(Countries=="Singapore")
 head(Singapore)
-
+#Generated the graph
 ggplot(Singapore, aes(x=Year, y=Investment))+
   geom_line() +
   geom_text(label=Singapore$Investment, size=3)
 
-
+#Created a table containing China's investment in different countries in 2014
 Invest14 <- DATA %>%
   filter(Year == "2014")
 head(Invest14)
-
+#Generated the graph
 Invest14 <- Invest14 %>%
   mutate(perc = Investment/sum(Investment))
 Invest14 = Invest14[order(Invest14$Investment, decreasing = TRUE),]
-
+#Beautify the graph
 myLabel = paste(Invest14$Countries, "(", round(Invest14$perc*100,2),"%)", sep = "")
 ggplot(Invest14, aes(x="", y=perc, fill=Countries)) +
   geom_bar(stat="identity",width=1) +
